aeb6e9d7e075cacbd98ebb8f7e3135023fbfda27
walid-elaazmani <walid.elaazmani@gmail.com>
2023-05-11 13:52:58 +0200

    Start

---
+++ src/main/java/be/intecbrussel/Account.java
@@ -0,0 +1,37 @@
+package be.intecbrussel;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class Account {
+    private String accountNumber;
+
+    public Account(String accountNumber) {
+        setAccountNumber(accountNumber);
+    }
+
+    private void setAccountNumber(String accountNumber) {
+
+        if (accountNumber.length() == 12 && accountNumber.matches("[0-9]+")){
+
+            long splitNumber1 = Long.parseLong(accountNumber.substring(0,10));
+            long splitNumber2 = Long.parseLong(accountNumber.substring(10,12));
+
+            if ((splitNumber1) % 97 == (splitNumber2)){
+                this.accountNumber = String.valueOf(accountNumber).replaceFirst("(\\d{3})(\\d{7})(\\d{2})", "$1-$2-$3");
+            }
+       }
+        else throw new RuntimeException("The number is too short/long or contains other characters");
+    }
+
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public static void main(String[] args) {
+        Account account = new Account("056879652107");
+        System.out.println(account.getAccountNumber());
+    }
+}
---
+++ src/test/java/be/intecbrussel/AccountTest.java
@@ -0,0 +1,60 @@
+package be.intecbrussel;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+
+    @Test
+    void ANumberThatFulfillsTheRulesShouldDisplayTheNumber(){
+
+        Account account = new Account("456879652106");
+
+        String expectedNumber = "456-8796521-06";
+
+        String actualNumber = account.getAccountNumber();
+
+        assertEquals(expectedNumber, actualNumber);
+
+    }
+
+    @Test
+    void ANumberOfWhichTheTwoLastNumbersAreNotEqualToTheRestOfTheModuloOfTheTenFirstNumbersBy97ShouldReturnNull(){
+
+        Account account = new Account("456879652108");
+
+        String expectedNumber = null;
+
+        String actualNumber = account.getAccountNumber();
+
+        assertNull(actualNumber);
+    }
+
+
+    @Test
+    public void testUsernameTooShortShouldThrowAnException() {
+        Throwable exception = assertThrows(
+                RuntimeException.class, () -> {
+                    Account user = new Account("45");
+                }
+        );
+
+        assertEquals("The number is too short/long or contains other characters", exception.getMessage());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
